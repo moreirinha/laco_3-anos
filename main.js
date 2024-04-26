cdeb4aeafa8bcbc7563b423fe41d501bf6545449
moreirinha <108348140+moreirinha@users.noreply.github.com>
2024-04-26 09:38:17 -0300

    e

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function verificarTime() {
     var respostaTime = prompt("Qual os melhor time?");
 
     
-    if (respostaTime.toLowerCase() === "Athlético PR") {
+    if (respostaTime.toLowerCase() === "moreira") {
       alert("Isso mesmo! O Athlético PR é o melhor time!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
